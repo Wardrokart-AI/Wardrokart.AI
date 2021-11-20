525471315417679cdc0a9e03812c99f99deeb3bc
rishabhkeshan <rishabh.keshan2019@vitstudent.ac.in>
2021-11-20 20:34:26 +0530

    added checkout and cart details page

--- wardrokart/views.py
+++ wardrokart/views.py
@@ -63,6 +63,138 @@ def convert(s):
 
 
     return s
+@csrf_exempt
+def myajaxtestview(request):
+    # print(x + "OH YEAH")
+    # print('myajaxtestview')
+    # if(request.POST['text']):
+    # dict = {"one":"1" , "two":"2"}
+    print("hii")
+    x = Run.main()
+    x = convert(x)
+    print(x)
+
+    print(request)
+    # print("OH YEAH"+x)
+    s = request
+    if(sb.temp > 0 or s == "checkout"):
+        # print("Checking out")
+        sb.temp = sb.temp + 1
+        if(sb.temp == 1):
+            # print("Showing list")
+            resp = sb.handle("show list")
+            resp,sb.other = resp
+
+            sb.address = s
+            for product,quantity in sb.other:
+                print("YO")
+                p1 = Product.objects.filter(slug = product)[0]
+                p = Product.objects.get(productid=p1.productid)
+
+                cart = Cart(request)
+
+                cart.add(product = p , quantity = quantity)
+            resp = "Adding... \nCheck CART"
+            sb.handle("clear list")
+            sb.temp = 0
+             # print(other)
+        data = json.dumps({
+            'response': resp,
+            'user': x,
+        })
+        return HttpResponse(data)
+
+    resp = sb.handle(x)
+    if(resp == None):
+        # print("Added")
+        resp = "Done..."
+    else:
+        print(resp)
+    data = json.dumps({
+        'response': resp,
+        'user': x,
+    })
+
+    return HttpResponse(data)
+
+
+@csrf_exempt
+def myajaxtestviewtext(request):
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    print(request.POST['text'])
+    print("^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^")
+    s = str(request.POST['text'])
+    sb.temp = 0
+    if(sb.temp > 0 or s == "checkout"):
+        # print("Checking out")
+        sb.temp = sb.temp + 1
+        if(sb.temp == 1):
+            resp = sb.handle("show list")
+            resp , sb.other = resp
+
+            sb.address = s
+            flag=0
+            for product,quantity in sb.other:
+                print("YO")
+                p1 = Product.objects.filter(slug = product)[0]
+                p = Product.objects.get(productid=p1.productid)
+                cart = Cart(request)
+                qty = p.quantity
+                if quantity>qty:
+                    resp = "Number of Items exceeded the stock..\nPlease Clear the list and try again."
+                    return HttpResponse(resp)
+                p.quantity = qty-quantity
+                p.save()
+                cart.add(product = p , quantity = quantity)
+               
+            resp = "Adding... \nCheck CART"
+            sb.handle("clear list")
+            sb.temp = 0
+             # print(other)
+        return HttpResponse(resp)
+    if(s == "clear cart" or s=="empty cart"):
+        cart = Cart(request)
+        cart.clear()
+        resp = "CLEARING CART"
+
+             # print(other)
+        return HttpResponse(resp)
+    resp = sb.handle(s)
+
+    # print(resp)
+
+    if type(resp) is tuple:
+        resp,other = resp
+        print(resp)
+
+    # print("***********************")
+    if(resp == "None"):
+            resp = "Done..."
+    else:
+        print(resp)
+    # print("***********************")
+    return HttpResponse(resp)
+
+def ordering():
+    if(sb.temp == 1):
+        resp = sb.handle("show list")
+        resp,sb.other = resp
+        resp = "Processing.... \nEnter your phone number:"
+    elif(sb.temp == 2):
+        sb.phone = int(s)
+        resp = "Processing.... \nEnter your address:"
+    else:
+        sb.address = s
+        for product,quantity in sb.other:
+            p = Product.objects.get(slug=product)
+            u = User.objects.get(username=request.user.username)
+            o = Order(item=product,quantity=quantity,price=p.price,total=(int(quantity)*int(p.price)),name=request.user.username,phone=sb.phone,email=u.email,address=sb.address,user_id=u.id)
+            o.save()
+        resp = "Adding... \nCheck Dashboard"
+        sb.handle("clear list")
+        sb.temp = 0
+    # print(other)
+    return HttpResponse(resp)
 
 
 
