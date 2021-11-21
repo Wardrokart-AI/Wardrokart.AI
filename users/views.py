86f572196cc399026a8d80d33837c15b787d65d9
TalionSlade <arpan01ghosh1307@gmail.com>
2021-11-21 16:19:46 +0530

    seller functionalities integrated

--- users/views.py
+++ users/views.py
@@ -10,7 +10,7 @@ from django.core.paginator import Paginator
 from django.contrib.auth import login
 # from decorators import customer_required
 from django.contrib.auth.decorators import login_required
-import users.face_detect as face_detect
+# import users.face_detect as face_detect
 from users.models import Profile,User,UserUploads
 from orders.models import Order
 from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
@@ -21,7 +21,7 @@ from users.forms import SellerSignUpForm
 import base64
 from PIL import Image
 from io import BytesIO
-import cv2
+# import cv2
 import numpy as np
 from django.core.files import File
 from django.core.files.base import ContentFile
@@ -101,7 +101,7 @@ def login_customer(request):
 			# is_seller = User.objects.filter(username = username).values("is_seller")[0]["is_seller"]
 
 			if user is not None and is_customer:
-				res = face_detect.check(user)
+				# res = face_detect.check(user)
 				res = True
 				if res:
 					auth.login(request,user)
@@ -112,7 +112,7 @@ def login_customer(request):
 
 
 			elif user is not None and is_seller:
-				res = face_detect.check(user)
+				# res = face_detect.check(user)
 				res = True
 				if res:
 					auth.login(request,user)
@@ -144,6 +144,33 @@ def dashboard_customer(request):
 	}
 	return render(request,'users/dashboard.html',context)
 
+def dashboard_seller(request):
+	order_seller = []
+	context={
+		'Orders':'',
+		'Seller':'',
+	}
+	for i in range(100):
+		if(Order.objects.filter(productid = i)):
+			if(Product.objects.filter(productid =i).values()[0]['user_id'] == request.user.id):
+				order = Order.objects.filter(productid = i).values()
+				order_seller.append(order)
+				x = order_seller[0]
+				context={
+
+					'Order':order_seller,
+					'Seller':x,
+				}
+	return render(request,'seller/sellerorder.html',context)
+		# 	else:
+		# 		return redirect('users:home')
+		# else:
+		# 	return redirect('users:home')
+
+	# if(None):
+	# 	return redirect('users:home')
+
+	# product_object = Product.objects.filter(productid ="1").values()[0]['user_id']
 
 
 @login_required(login_url="/users/login")
@@ -191,4 +218,126 @@ def logout_customer(request):
 		messages.success(request,'You Are Now Logged Out')
 		return redirect('pages:home')
 
+class SellerSignUpView(CreateView):
+    model = User
+    form_class = SellerSignUpForm
+    template_name = 'seller/signup_form.html'
 
+    def get_context_data(self, **kwargs):
+        kwargs['user_type'] = 'Seller'
+        return super().get_context_data(**kwargs)
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('/shop')
+
+@login_required(login_url="/users/login")
+def seller_product(request):
+	if(request.user):
+		if(request.user.is_seller):
+			user_id = request.user.id
+			categories = Category.objects.all()
+			products = Product.objects.filter(user =user_id)
+
+			context = {
+
+				# 'title':request.user.name,
+				'products':products,
+				'category':categories,
+			}
+			return render(request ,'seller/home.html' , context)
+		else:
+			return render(request,'users/login.html')
+
+@login_required(login_url="/users/login")
+def seller_product_add(request):
+	if(request.user):
+		if(request.user.is_seller):
+			user_id = request.user.id
+
+			# categories1 = categories[0]
+			categories = Category.objects.all()
+			context = {
+				'category':categories,
+				# 'title':request.user.name,
+				# 'products':"products"
+			}
+			if request.method == 'POST':
+				try:
+					image = request.FILES['image']
+				except MultiValueDictKeyError:
+					image = False
+				title = request.POST['title']
+				price = request.POST['price']
+				description = request.POST['description']
+				quantity = request.POST['quantity']
+				category = request.POST.get('category1')
+				if category == '--Select Category--':
+					message.error(request,"Select Category!")
+					return render(request ,'seller/add.html' , context)
+				elif category is None:
+					category = request.POST['category'].upper()
+				else:
+					category = category.upper()
+				try:
+
+					catg = Category.objects.get(title=category)
+				except Category.DoesNotExist:
+					cat = Category.objects.create(
+						title=category,
+						slug=category
+					)
+					cat.save()
+				catg = Category.objects.get(title=category)
+				if True:
+					slug = title.lower()
+					prod = Product.objects.create(user=request.user ,
+							title = title ,
+							slug = slug,
+							price = price ,
+							quantity = quantity ,
+							description=description ,
+							photo = image ,
+							category = catg
+						)
+					prod.save()
+					messages.success(request , 'PRODUCT ADDED')
+					return redirect('users:home')
+			# products = Product.objects.filter(user =user_id)
+			# print(products)
+
+			return render(request ,'seller/add.html' , context)
+		else:
+			return render(request,'users/login.html')
+
+@login_required(login_url="/users/login")
+def seller_product_detail(request,product_id):
+	product = get_object_or_404(Product,productid=product_id)
+	if request.method == 'POST':
+		price = request.POST['price']
+		description = request.POST['description']
+		quantity = request.POST['quantity']
+		# product = get_object_or_404(Product,pk=product_id)
+		if product is not None:
+			product.price = price
+			product.description = description
+			product.quantity = quantity
+			product.save()
+			messages.success(request,'Product Updated Successfully')
+			return redirect('users:home')
+		else:
+			messages.error(request,'Product Not Found')
+			return redirect('users:product_detail', product_id = product_id)
+	else:
+		return render(request,'seller/product_detail.html', {'product':product})
+
+def deliver_order(request, order_id):
+	try:
+		order = Order.objects.get(pk=order_id)
+	except:
+		messages.info(request, "Order Not Found")
+		return redirect('users:order')
+	order.delivery = True
+	order.save()
+	return redirect('users:order')
